3cbf073ab51cd8a605899806a4dbc00003dd6c5e
Nikola0790 <nikola.drcalic@gmail.com>
2021-12-27 16:07:05 +0100

    api by city and api by geolocation

---
+++ src/service/service.jsx
@@ -0,0 +1,18 @@
+const apiKey = "ls";
+const url = "https://api.openweathermap.org/data/2.5/";
+/* ("https://api.openweathermap.org/data/2.5/weather?q=Ljubis&appid=12b42cefab96f10f922e9990cbf21bc4&units=metric"); */
+
+export const getDataByCityName = (city) => {
+  return fetch(`${url}weather?q=${city}&appid=${apiKey}&units=metric`).then(
+    (response) => {
+      return response.json();
+    }
+  );
+};
+/* https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} */
+
+export const getDataByGeolocation = (latitude, longitude) => {
+  return fetch(
+    `${url}onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+  ).then((response) => response.json());
+};
